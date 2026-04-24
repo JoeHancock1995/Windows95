c1d7dc4c20d264ce47e8797bd65c7847bce1eb2a
Joseph Hancock <joe.hancock95@gmail.com>
2026-04-24 15:17:47 -0400

    Create Windows 95-style desktop web app UI

--- public/static/js/views/Artgallery.js
+++ public/static/js/views/Artgallery.js
@@ -3,18 +3,25 @@ import AbstractView from "./AbstractView.js";
 export default class extends AbstractView {
     constructor(params) {
         super(params);
-        this.setTitle("Home");
+        this.setTitle("Art Gallery");
     }
 
     async getHtml() {
         return `
-            <h1>Data Gallery</h1>
-            <p>
-               small showcase of some art work within a 3d gallery
-            </p>
-            <p>
-                // <a href="/other" data-link>View all</a>.
-            </p>
+            <section class="window">
+                <header class="window__titlebar">
+                    <span>Art Gallery</span>
+                    <div class="window__controls" aria-hidden="true">
+                        <span class="window__control">_</span>
+                        <span class="window__control">▢</span>
+                        <span class="window__control">✕</span>
+                    </div>
+                </header>
+                <div class="window__body">
+                    <p>Small showcase of art work presented in a gallery style experience.</p>
+                    <p>Use the Start menu to jump back to the desktop anytime.</p>
+                </div>
+            </section>
         `;
     }
 }
